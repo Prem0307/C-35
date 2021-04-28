2804be678befda7526c2a4e843a195fe66ce73fd
Prem0307 <76106328+Prem0307@users.noreply.github.com>
2021-04-28 14:50:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball;
+var database,pos;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballref=database.ref('ball/position');
+    ballref.on("value",readdata,showerror);
+}
+function readdata(data){
+pos=data.val();
+ball.x= pos.x;
+ball.y=pos.y;
+}
+function showerror(){
+console.log("error")
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+   // ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        x:pos.x+x,
+        y:pos.y+y
+    });
+}
+
